8b809c79751aee1ecd69b46a5acd4b25e5786b84
pereeserra <pereserra@paucasesnovescifp.cat>
2026-05-06 12:19:22 +0200

    Adapta servidor backend al port de Railway

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,8 @@
 require("./config/env");
 const app = require("./app");
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 // Codi per iniciar el servidor
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor executant-se al port ${PORT}`);
 });
