19e0ff2fed9b371b7a3b4dba6bf3739d2a9b863a
pereeserra <pereserra@paucasesnovescifp.cat>
2026-05-06 10:28:34 +0200

    Preparar connexió MySQL per desplegament en producció

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,11 +3,16 @@ require("dotenv").config();
 // Configuración de la conexión a la base de datos MySQL utilizando un pool de conexiones
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "padelbook",
   waitForConnections: true,
   connectionLimit: 10,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : undefined,
 });
 
 module.exports = pool;
